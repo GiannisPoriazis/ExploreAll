4f6dea7e9c317a0921521c6e117b24b80eae020a
Stefanos Poriazis <sporiazis@wheelsys.com>
2022-06-15 17:32:20 +0300

    Added helper class

---
+++ ExploreAll/ExploreAllHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExploreAll
+{
+    public class WebMethodResult
+    {
+        public string data { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}
